d7c4a81747f2a342e106e6ac5ef3dccdb6a93fa7
MattChu5566 <q850722@gmail.com>
2024-02-09 17:07:48 +0800

    HW0B

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -5,41 +5,69 @@ public class JavaExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
-        // TODO: Fill in this function.
-        return null;
-    }
-
-    /** Returns the order depending on the customer.
-     *  If the customer is Ergun, return ["beyti", "pizza", "hamburger", "tea"].
-     *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
-     *  In any other case, return an empty String[] of size 3. */
-    public static String[] takeOrder(String customer) {
-        // TODO: Fill in this function.
-        return null;
-    }
-
-    /** Returns the positive difference between the maximum element and minimum element of the given array.
-     *  Assumes array is nonempty. */
-    public static int findMinMax(int[] array) {
-        // TODO: Fill in this function.
-        return 0;
+        int[] dice = { 1, 2, 3, 4, 5, 6 };
+        return dice;
     }
 
     /**
-      * Uses recursion to compute the hailstone sequence as a list of integers starting from an input number n.
-      * Hailstone sequence is described as:
-      *    - Pick a positive integer n as the start
-      *        - If n is even, divide n by 2
-      *        - If n is odd, multiply n by 3 and add 1
-      *    - Continue this process until n is 1
-      */
+     * Returns the order depending on the customer.
+     * If the customer is Ergun, return ["beyti", "pizza", "hamburger", "tea"].
+     * If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
+     * In any other case, return an empty String[] of size 3.
+     */
+    public static String[] takeOrder(String customer) {
+        String[] ErgunOrder = { "beyti", "pizza", "hamburger", "tea" };
+        String[] ErikOrder = { "sushi", "pasta", "avocado", "coffee" };
+        String[] emptyOrder = { "", "", "" };
+        if (customer.equals("Ergun"))
+            return ErgunOrder;
+        if (customer.equals("Erik"))
+            return ErikOrder;
+        return emptyOrder;
+    }
+
+    /**
+     * Returns the positive difference between the maximum element and minimum
+     * element of the given array.
+     * Assumes array is nonempty.
+     */
+    public static int findMinMax(int[] array) {
+        int min = array[0];
+        int max = array[0];
+        for (int i : array) {
+            if (i < min)
+                min = i;
+            if (i > max)
+                max = i;
+        }
+        return max - min;
+    }
+
+    /**
+     * Uses recursion to compute the hailstone sequence as a list of integers
+     * starting from an input number n.
+     * Hailstone sequence is described as:
+     * - Pick a positive integer n as the start
+     * - If n is even, divide n by 2
+     * - If n is odd, multiply n by 3 and add 1
+     * - Continue this process until n is 1
+     */
     public static List<Integer> hailstone(int n) {
         return hailstoneHelper(n, new ArrayList<>());
     }
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
-        // TODO: Fill in this function.
-        return null;
+        list.add(x);
+        int result;
+        if (x % 2 == 0) {
+            result = x / 2;
+            return hailstoneHelper(result, list);
+        } else if (x == 1) {
+            return list;
+        } else {
+            result = 3 * x + 1;
+            return hailstoneHelper(result, list);
+        }
     }
 
 }
--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,29 +1,52 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
-	public static int sum(List<Integer> L) {
-        // TODO: Fill in this function.
-        return 0;
+    public static int sum(List<Integer> L) {
+        int result = 0;
+        for (int i : L) {
+            result += i;
+        }
+        return result;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> evenList = new ArrayList<>();
+        for (int i : L) {
+            if (i % 2 == 0) {
+                evenList.add(i);
+            }
+        }
+        return evenList;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> commonList = new ArrayList<>();
+        for (int i : L1) {
+            if (L2.contains(i)) {
+                commonList.add(i);
+            }
+        }
+        return commonList;
     }
 
-
-    /** Returns the number of occurrences of the given character in a list of strings. */
+    /**
+     * Returns the number of occurrences of the given character in a list of
+     * strings.
+     */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        // TODO: Fill in this function.
-        return 0;
+        int count = 0;
+        for (String s : words) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == c) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
